f5bee3f903a605c60c23d45393c2e150b4302100
EDGAR CAMACHO SEABRA RIBEIRO <081230039@faculdade.cefsa.edu.br>
2026-05-04 22:14:46 -0300

    first commit

---
+++ NRZ-L/Projeto NRZ-L/Projeto NRZ-L.srcs/sim_1/new/tb_nrzl.vhd
@@ -0,0 +1,49 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity nrz_l_encoder_tb is
+end nrz_l_encoder_tb;
+
+architecture sim of nrz_l_encoder_tb is
+    -- Timing constants
+    constant CLK_PERIOD : time := 2 ns;
+    constant BIT_PERIOD : time := 20 ns;
+    constant TICKS_VAL  : integer := 10;
+
+    signal clk_tb     : STD_LOGIC := '0';
+    signal reset_tb   : STD_LOGIC := '0';
+    signal data_in_tb : STD_LOGIC_VECTOR(15 downto 0) := "1100101011110001";
+    signal nrz_out_tb : STD_LOGIC;
+
+begin
+    uut: entity work.nrz_l_encoder
+        generic map (
+            TICKS_PER_BIT => TICKS_VAL
+        )
+        port map (
+            clk     => clk_tb,
+            reset   => reset_tb,
+            data_in => data_in_tb,
+            nrz_out => nrz_out_tb
+        );
+
+    clk_process : process
+    begin
+        while true loop
+            clk_tb <= '0'; wait for CLK_PERIOD/2;
+            clk_tb <= '1'; wait for CLK_PERIOD/2;
+        end loop;
+    end process;
+
+    stim_proc: process
+    begin		
+        reset_tb <= '1';
+        wait for 10 ns;
+        reset_tb <= '0';
+
+        -- Observation time for the 16-bit sequence
+        wait for 16 * BIT_PERIOD;
+
+        wait;
+    end process;
+end sim;
